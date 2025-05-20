13d7cbe8cbb20d3b833b119d622be138796e0266
ninshot <140782230+ninshot@users.noreply.github.com>
2025-05-19 20:26:57 -0600

    Addition of nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import { useState } from 'react'
+/**
+ * The main entry point for the 3D portfolio application.
+ * This component renders the main application layout and structure.
+ * 
+ */
 
-function App() {
+import {BrowserRouter} from 'react-router-dom';
+import {About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas} from './components';
+
+
+const App = () => {
   return (
-    <div>
-      3D Portfolio
-    </div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+      </div>
+    </BrowserRouter>
     
   )
 }
