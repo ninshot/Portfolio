c8f3d9be5963e41918db64e7d3e93c24f7c78004
ninshot <140782230+ninshot@users.noreply.github.com>
2025-06-12 15:19:39 -0600

    Addition of computer component on the main page

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+/**
+ * Canvas component for rendering 3D models using React Three Fiber.
+ * Suspense is used to handle loading states.
+ * OrbitControls allows for interactive camera control using mouse or touch gestures.
+ * Preload is used to load assets before rendering helps in rendering objects when the startup is slow.
+ * CanvasLoader is a custom loader component that displays a loading animation while the 3D model is being loaded.
+ */
+import {Canvas} from '@react-three/fiber';
+import {Suspense,useState, useEffect} from 'react';
+import {OrbitControls, Preload, useGLTF} from '@react-three/drei';
+//import CanvasLoader from './CanvasLoader';
 
 const Computers = () => {
+  // useGLTF is a hook from @react-three/drei that loads GLTF models which are 3D models in a specific format.
+  const computer = useGLTF('./desktop_pc/scene.gltf')
   return (
-    <div>Computers</div>
+      
+      <mesh> //this tag is used to create a 3D object in the scene
+
+      </mesh>
   )
 }
 
