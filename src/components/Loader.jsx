fde5e0e5692b2e6bc5aea0c7930982ee826edebc
ninshot <140782230+ninshot@users.noreply.github.com>
2025-06-13 01:58:39 -0600

    Addition of a loading information page and 3-d computer model on the landing page

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,9 +1,26 @@
+/**
+ * Loader component to display loading progress in a 3D scene.
+ * It uses the useProgress hook from @react-three/drei to get the loading progress.
+ * The Html component is used to display HTML content in the 3D scene.
+ */
 import React from 'react'
+import {Html, useProgress} from '@react-three/drei'; 
 
-const Loader = () => {
+const  Loader = () => {
+  const { progress } = useProgress();
   return (
-    <div>Loader</div>
+    <Html>
+      <span className='canvas-load'> </span>
+       <p
+        style={{
+          fontSize: 14,
+          color: '#f1f1f1',
+          fontWeight: 800,
+          marginTop: 40}}
+       >{progress.toFixed(2)}%</p>
+
+    </Html>
   )
 }
 
-export default Loader
+export default Loader;
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -9,17 +9,53 @@ import React from 'react';
 import {Canvas} from '@react-three/fiber';
 import {Suspense,useState, useEffect} from 'react';
 import {OrbitControls, Preload, useGLTF} from '@react-three/drei';
-//import CanvasLoader from './CanvasLoader';
+import CanvasLoader from '../Loader';
 
 const Computers = () => {
   // useGLTF is a hook from @react-three/drei that loads GLTF models which are 3D models in a specific format.
   const computer = useGLTF('./desktop_pc/scene.gltf')
   return (
-      
-      <mesh> //this tag is used to create a 3D object in the scene
-
+      //this tag is used to create a 3D object in the scene
+      <mesh> 
+        <hemisphereLight intensity={0.15} groundColor="black" /> {/* Adds ambient light to the scene */}
+        <pointLight intensity={1} /> {/* Adds a point light to the scene */}
+        <spotLight
+          position={[-20,50,10]}
+          angle={0.12} //sets the angle of the spotlight
+          penumbra={1} //sets the softness of the spotlight's edges
+          intensity={1} //sets the brightness of the spotlight
+          castShadow //allows the spotlight to cast shadows
+          shadow-mapSize={1024} //sets the size of the shadow map for better shadow quality
+        />
+        <primitive
+          object={computer.scene} //the 3D computer model is added to the scene
+          scale={0.75}
+          position={[0, -3.25, -1.5]} //sets the position of the computer model in the 3D space 
+          rotation={[-0.01, -0.2, -0.1]} //sets the rotation of the computer model
+          />
       </mesh>
   )
 }
 
-export default Computers
+const ComputersCanvas = () => {
+  return(
+    <Canvas
+      frameloop = "demand"
+      shadows
+      camera = {{position: [20, 3, 5], fov: 25}}  
+      gl={{preserveDrawingBuffer: true}} //preserves the drawing buffer for better rendering quality
+      >
+      <Suspense fallback={<CanvasLoader />} >
+        <OrbitControls 
+          enableZoom={false} //disables zooming in and out
+          maxPolarAngle={Math.PI / 2} //limits the camera's vertical rotation to prevent flipping
+          minPolarAngle={Math.PI / 2} //limits the camera's vertical rotation to prevent flipping
+        />
+        <Computers /> {/* Renders the Computers component which contains the 3D model */}
+      </Suspense>
+      <Preload all /> {/* Preloads all assets to improve performance */}
+    </Canvas>
+  )
+}
+
+export default ComputersCanvas;
