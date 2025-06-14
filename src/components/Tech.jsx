401f3efcb582e9d800f0000fec9983b9964111f2
ninshot <140782230+ninshot@users.noreply.github.com>
2025-06-13 18:35:20 -0600

    Addition of skills section

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import { motion } from 'framer-motion'
+import { styles } from '../styles'
+import { SectionWrapper } from '../hoc'
+import { textVariant } from '../utils/motion'
+import { technologies } from '../constants'
+
+const TechnologyCard = ({text}) => {
+  return(
+    <p> {text}</p>
+
+  )
+
+}
 
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <>
+    
+      <motion.div variants={textVariant()}>
+          <p className={styles.sectionSubText}>
+             </p>
+          <h2 className={styles.heroHeadText}> SKILLS</h2>
+      </motion.div>
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {technologies.map((technology, idx) => (
+          <TechnologyCard text = {technology} key={idx}/>
+        ))}
+      </div>
+    </>
   )
 }
 
-export default Tech
+export default SectionWrapper(Tech, "tech");
--- src/constants/index.js
+++ src/constants/index.js
@@ -33,58 +33,19 @@ import {
   ];
   
   const technologies = [
-    {
-      name: "HTML 5",
-      icon: html,
-    },
-    {
-      name: "CSS 3",
-      icon: css,
-    },
-    {
-      name: "JavaScript",
-      icon: javascript,
-    },
-    {
-      name: "TypeScript",
-      icon: typescript,
-    },
-    {
-      name: "React JS",
-      icon: reactjs,
-    },
-    {
-      name: "Redux Toolkit",
-      icon: redux,
-    },
-    {
-      name: "Tailwind CSS",
-      icon: tailwind,
-    },
-    {
-      name: "Node JS",
-      icon: nodejs,
-    },
-    {
-      name: "MongoDB",
-      icon: mongodb,
-    },
-    {
-      name: "Three JS",
-      icon: threejs,
-    },
-    {
-      name: "git",
-      icon: git,
-    },
-    {
-      name: "figma",
-      icon: figma,
-    },
-    {
-      name: "docker",
-      icon: docker,
-    },
+    "Python",
+    "Java",
+    "JavaScript",
+    "HTML",
+    "CSS",
+    "React",
+    "Node.js",
+    "Express.js",
+    "MySQL",
+    "C#",
+    "C",
+    "Rust",
+    "Docker"
   ];
   
   
