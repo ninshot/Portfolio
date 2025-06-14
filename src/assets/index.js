536476865e2c4f7c9b2c0f1a701903d45ad23adb
ninshot <140782230+ninshot@users.noreply.github.com>
2025-06-14 17:48:40 -0600

    Addition of images for languages

--- src/assets/index.js
+++ src/assets/index.js
@@ -16,17 +16,13 @@ import WW3 from "./WW3.png";
 
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
-import figma from "./tech/figma.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
 import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
-import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
-import typescript from "./tech/typescript.png";
-import threejs from "./tech/threejs.svg";
+
 
 
 export {
@@ -36,17 +32,12 @@ export {
   close,
   css,
   docker,
-  figma,
   git,
   html,
   javascript,
-  mongodb,
   nodejs,
   reactjs,
-  redux,
   tailwind,
-  typescript,
-  threejs,
   CC1,
   CC2,
   CC3,
